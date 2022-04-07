09a8f4bc69e9cfce2d6bfd7a0f099a131f5c5d70
Roberta Assis de Carvalho <roberta.assisdecarvalho9@gmail.com>
2022-04-07 13:55:07 -0300

    Ciclo de vida de uma entidade - detached, managed

--- loja/src/main/java/br/com/alura/loja/dao/CategoriaDao.java
+++ loja/src/main/java/br/com/alura/loja/dao/CategoriaDao.java
@@ -16,5 +16,14 @@ public class CategoriaDao {
 		this.em.persist(categoria); // insere na tabela
 	}
 	
+	public void atualizar(Categoria categoria) {
+		this.em.merge(categoria); // garante que vai pro estado managed pra fazer o set futuramente
+	}
+	
+	public void remover(Categoria categoria) {
+		categoria = em.merge(categoria); // precisa atribuir a uma variavel pois o (categoria) esta detached; so fica managed a variavel atribuida
+		this.em.remove(categoria); // precisa estar em managed pra dar certo de fazer remove
+	}
+	
 	
 }
